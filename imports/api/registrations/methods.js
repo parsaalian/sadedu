88286bfa83f2b879e4fee0c7187e070e6f7bf8cf
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2019-06-03 17:37:00 +0430

    registrations.remove method added

--- imports/api/registrations/methods.js
+++ imports/api/registrations/methods.js
@@ -8,5 +8,17 @@ Meteor.methods({
     } else {
       throw new Meteor.Error('This student already has this course.');
     }
+  },
+
+  'registrations.remove'({cid, sid}) {
+    if (Registrations.findOne({cid: cid, sid: sid})) {
+      Registrations.remove({cid, sid});
+    } else if (!Registrations.findOne({cid: cid})) {
+      throw new Meteor.Error('This course doesn\'t exist.');
+    } else if (!Registrations.findOne({sid: sid})) {
+      throw new Meteor.Error('This student doesn\'t exist.');
+    } else {
+      throw new Meteor.Error('This student doesn\'t take this course.');
+    }
   }
 });
