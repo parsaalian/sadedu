104052a2948a0f7279c04dad916167ef8f04d03c
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2019-06-03 17:31:52 +0430

    setting roles after adding users

--- imports/startup/users.js
+++ imports/startup/users.js
@@ -1,4 +1,16 @@
 import { Accounts } from "meteor/accounts-base";
+import { Roles } from "meteor/alanning:roles";
+import { ROLES } from "./roles";
+
+Meteor.users.after.insert(function (userID, doc) {
+  if (doc.profile.type === "admin") {
+    Roles.addUsersToRoles(doc._id, [ROLES.Admin]);
+  } else if (doc.profile.type === "student") {
+    Roles.addUsersToRoles(doc._id, [ROLES.Student]);
+  } else if (doc.profile.type === "assistant") {
+    Roles.addUsersToRoles(doc._id, [ROLES.Assistant]);
+  }
+});
 
 if (!Accounts.findUserByUsername("admin")) {
   Accounts.createUser({username: "admin", password: "admin", profile: {type: "admin"}});
