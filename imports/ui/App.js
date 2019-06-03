028a0fd509446a1f6e25d2045b443c8946bf6017
ce443-972-95104866 <yasamin.tabatbaee@gmail.com>
2019-06-03 13:19:52 +0430

    Login page front

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,7 +1,7 @@
-import { Meteor } from "meteor/meteor";
-import React, { Component } from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import { Students } from "/imports/api/students/students";
+import {Meteor} from "meteor/meteor";
+import React, {Component} from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import {Students} from "/imports/api/students/students";
 
 import Login from "./login/login";
 // admin panel pages
@@ -10,16 +10,16 @@ import AdminStudents from "./admin/students/students";
 import AdminCourses from "./admin/courses/courses";
 
 class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        {/*<Route path="/" exact component={Login} />*/}
-        <Route path="/" exact component={AdminHome} />
-        <Route path="/admin/students" exact component={AdminStudents} />
-        <Route path="/admin/courses" exact component={AdminCourses} />
-      </BrowserRouter>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Route path="/" exact component={Login}/>
+                {/*<Route path="/" exact component={AdminHome} />*/}
+                <Route path="/admin/students" exact component={AdminStudents}/>
+                <Route path="/admin/courses" exact component={AdminCourses}/>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
--- imports/ui/login/form/form.js
+++ imports/ui/login/form/form.js
@@ -1,28 +1,50 @@
-import React, { Component } from "react";
-import { Form, Button, Alert } from "react-bootstrap";
+import React, {Component} from "react";
+import {Form, Button, Alert} from "react-bootstrap";
+import {LinkedInLoginButton, GoogleLoginButton} from "react-social-login-buttons";
 import Input from "/imports/ui/_global/input";
+import './login.css'
 
 export default class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+    }
 
-  login(e) {
-    e.preventDefault();
-    this.props.login(this.refs.username.state.value, this.refs.password.state.value);
-  }
+    login(e) {
+        e.preventDefault();
+        this.props.login(this.refs.username.state.value, this.refs.password.state.value);
+    }
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.props.error !== "" ? <Alert color="danger">{this.props.error}</Alert> : <span></span>}
-        <Form>
-          <Input name="username" type="text" ref="username" label="Username" />
-          <Input name="password" type="password" ref="password" label="Password" />
-        </Form>
-        <Button color="primary" onClick={this.login}>Login</Button>
-      </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <form className="demoForm">
+                <h4>ورود با حساب کاربری</h4>
+
+                <div className={`form-group`}>
+                    <label htmlFor="username">نام کاربری</label>
+                    <input type="username" required className="form-control" name="username"
+                           placeholder="نام کاربری" align={"center"}/>
+                </div>
+                <div className={`form-group `}>
+                    <label htmlFor="password">کلمه عبور</label>
+                    <input type="password" className="form-control" name="password"
+                           placeholder="کلمه عبور"/>
+                </div>
+                <Form.Group controlId="formBasicChecbox">
+                    <Form.Check type="checkbox" label="مرا به خاطر بسپار"/>
+                </Form.Group>
+                <button type="submit" className="btn btn-info" onClick={this.login}>ورود</button>
+                <div>
+                    <a href='#'>بازیابی کلمه عبور</a>
+                </div>
+
+                <h4>ورود با حساب اجتماعی</h4>
+                <GoogleLoginButton size="50px" onClick={() => alert("Hello")}>
+                    <span>ورود با حساب گوگل</span>
+                </GoogleLoginButton>
+                <LinkedInLoginButton onClick={() => alert("Hello")}/>
+
+            </form>
+        );
+    }
 }
