26dc774134e56dd9e749637c3c8a6d8897b6d9b9
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2019-06-03 17:39:50 +0430

    registrations methods single quote to double quote

--- imports/api/registrations/methods.js
+++ imports/api/registrations/methods.js
@@ -2,27 +2,27 @@ import { Meteor } from "meteor/meteor";
 import { Registrations } from "./registrations";
 
 Meteor.methods({
-  'registrations.add'({cid, sid}) {
+  "registrations.add"({cid, sid}) {
     if (!Registrations.findOne({cid: cid, sid: sid})) {
       Registrations.insert({cid, sid});
     } else {
-      throw new Meteor.Error('This student already has this course.');
+      throw new Meteor.Error("This student already has this course.");
     }
   },
 
-  'registrations.remove'({cid, sid}) {
+  "registrations.remove"({cid, sid}) {
     if (Registrations.findOne({cid: cid, sid: sid})) {
       Registrations.remove({cid, sid});
     } else if (!Registrations.findOne({cid: cid})) {
-      throw new Meteor.Error('This course doesn\'t exist.');
+      throw new Meteor.Error("This course doesn\'t exist.");
     } else if (!Registrations.findOne({sid: sid})) {
-      throw new Meteor.Error('This student doesn\'t exist.');
+      throw new Meteor.Error("This student doesn\'t exist.");
     } else {
-      throw new Meteor.Error('This student doesn\'t take this course.');
+      throw new Meteor.Error("This student doesn\'t take this course.");
     }
   },
 
-  'registrations.removeAll'() {
+  "registrations.removeAll"() {
     Registrations.remove({});
   }
 });
