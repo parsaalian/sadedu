0b82b43022d4a309ae24d9cb7186ed4d4e826d68
y-tabatabaee76 <yasamin.tabatbaee@gmail.com>
2019-06-06 17:02:43 +0430

    Added a few statistics cards

--- imports/ui/assistant/panel/menu/menu.js
+++ imports/ui/assistant/panel/menu/menu.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "antd/dist/antd.css";
 import "./menu.css";
+import Table from "./../table/table"
+
 import { Layout, Menu, Breadcrumb, Icon } from "antd";
+import { Statistic, Card, Row, Col } from "antd";
 
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
+
+const Countdown = Statistic.Countdown;
+const deadline = Date.now() + 1000 * 60 * 60 * 24 * 2 + 1000 * 30; // Moment is also OK
+
+function onFinish() {
+    console.log("finished!");
+}
 
 export default class SiderDemo extends React.Component {
     state = {
@@ -68,6 +78,56 @@ export default class SiderDemo extends React.Component {
                             <Breadcrumb.Item>Courses</Breadcrumb.Item>
                             <Breadcrumb.Item>CE</Breadcrumb.Item>
                         </Breadcrumb>
+                        <div style={{ background: "#fff", padding: "30px" }}>
+                            <Row gutter={16}>
+                                <Col span={6}>
+                                    <Card style={{ width: 200 }} title="Student Requests">
+                                        <Statistic
+                                            value={11}
+                                            precision={0}
+                                            valueStyle={{ color: "#3f8600" }}
+                                            prefix={<Icon type="bell" theme="twoTone" twoToneColor="#52c41a" />}
+                                        />
+                                    </Card>
+                                </Col>
+
+                                <Col span={6}>
+                                    <Card style={{ width: 200 }} title="Remained Time">
+                                        <Countdown
+                                            value={deadline}
+                                            valueStyle={{ color: "#932381" }}
+                                            onFinish={onFinish}
+                                            prefix={
+                                                <Icon type="dashboard" theme="twoTone" twoToneColor="#932381" />
+                                            }
+                                        />
+                                    </Card>
+                                </Col>
+
+                                <Col span={6}>
+                                    <Card style={{ width: 200 }} title="Registerd Students">
+                                        <Statistic
+                                            value={40}
+                                            valueStyle={{ color: "#179ba1" }}
+                                            prefix={<Icon type="edit" theme="twoTone" twoToneColor="#179ba1" />}
+                                            suffix="/ 40"
+                                        />
+                                    </Card>
+                                </Col>
+
+                                <Col span={6}>
+                                    <Card style={{ width: 200 }} title="Reseved Students">
+                                        <Statistic
+                                            value={9}
+                                            valueStyle={{ color: "#f0931b" }}
+                                            prefix={<Icon type="edit" theme="twoTone" twoToneColor="#f0931b" />}
+                                            suffix="/ 15"
+                                        />
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                        <Table></Table>
                         <div style={{ padding: 24, background: "#fff", minHeight: 360 }}>
                             This is a sample page.
                         </div>
