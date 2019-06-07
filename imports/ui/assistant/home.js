5a58f2f7b66213d4038b0aea3f2b32d9933866c8
Parsa Alian <Parsa@Bardias-MacBook-Pro.local>
2019-06-07 15:14:48 +0430

    basics for chart

--- imports/ui/assistant/home.js
+++ imports/ui/assistant/home.js
@@ -1,9 +1,11 @@
 import { Meteor } from "meteor/meteor";
 import React, { Component } from "react";
-import Panel from "./panel/panel";
+import Chart from "./chart/chart";
+
+const boxStyle = { padding: "10px", border: "1px solid black", };
 
 export default class AssistantPage extends Component {
   render() {
-    return <Panel></Panel>;
+    return <Chart></Chart>
   }
 }
