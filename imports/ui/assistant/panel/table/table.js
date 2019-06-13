d63bd2641bde161b322f2fdc911108bfc70ab197
y-tabatabaee76 <yasamin.tabatbaee@gmail.com>
2019-06-13 11:27:47 +0430

    Added Status and Action to registration Table

--- imports/ui/assistant/panel/table/table.js
+++ imports/ui/assistant/panel/table/table.js
@@ -1,6 +1,6 @@
-import { Meteor } from "meteor/meteor";
-import React, { Component } from "react";
-import { Table, Input, Button, Icon } from "antd";
+import {Meteor} from "meteor/meteor";
+import React, {Component} from "react";
+import {Table, Input, Button, Icon, Divider, Tag, Popconfirm} from "antd";
 import Highlighter from "react-highlight-words";
 
 const data = [
@@ -9,24 +9,28 @@ const data = [
     name: "John Brown",
     age: 32,
     address: "New York No. 1 Lake Park",
+    tags: ['eligible'],
   },
   {
     key: "2",
     name: "Joe Black",
     age: 42,
     address: "London No. 1 Lake Park",
+    tags: ['eligible'],
   },
   {
     key: "3",
     name: "Jim Green",
     age: 32,
     address: "Sidney No. 1 Lake Park",
+    tags: ['eligible'],
   },
   {
     key: "4",
     name: "Jim Red",
     age: 32,
     address: "London No. 2 Lake Park",
+    tags: ['eligible'],
   },
 ];
 
@@ -36,8 +40,8 @@ export default class PanelTable extends Component {
   };
 
   getColumnSearchProps = dataIndex => ({
-    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
-      <div style={{ padding: 8 }}>
+    filterDropdown: ({setSelectedKeys, selectedKeys, confirm, clearFilters}) => (
+      <div style={{padding: 8}}>
         <Input
           ref={node => {
             this.searchInput = node;
@@ -46,24 +50,24 @@ export default class PanelTable extends Component {
           value={selectedKeys[0]}
           onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
           onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
-          style={{ width: 188, marginBottom: 8, display: "block" }}
+          style={{width: 188, marginBottom: 8, display: "block"}}
         />
         <Button
           type="primary"
           onClick={() => this.handleSearch(selectedKeys, confirm)}
           icon="search"
           size="small"
-          style={{ width: 90, marginRight: 8 }}
+          style={{width: 90, marginRight: 8}}
         >
           Search
         </Button>
-        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+        <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{width: 90}}>
           Reset
         </Button>
       </div>
     ),
     filterIcon: filtered => (
-      <Icon type="search" style={{ color: filtered ? "#1890ff" : undefined }} />
+      <Icon type="search" style={{color: filtered ? "#1890ff" : undefined}}/>
     ),
     onFilter: (value, record) =>
       record[dataIndex]
@@ -77,7 +81,7 @@ export default class PanelTable extends Component {
     },
     render: text => (
       <Highlighter
-        highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+        highlightStyle={{backgroundColor: "#ffc069", padding: 0}}
         searchWords={[this.state.searchText]}
         autoEscape
         textToHighlight={text.toString()}
@@ -87,28 +91,31 @@ export default class PanelTable extends Component {
 
   handleSearch = (selectedKeys, confirm) => {
     confirm();
-    this.setState({ searchText: selectedKeys[0] });
+    this.setState({searchText: selectedKeys[0]});
   };
 
   handleReset = clearFilters => {
     clearFilters();
-    this.setState({ searchText: "" });
+    this.setState({searchText: ""});
   };
 
   render() {
     const columns = [
       {
+        title: 'ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
         title: "Name",
         dataIndex: "name",
         key: "name",
-        width: "30%",
         ...this.getColumnSearchProps("name"),
       },
       {
         title: "Age",
         dataIndex: "age",
         key: "age",
-        width: "20%",
         ...this.getColumnSearchProps("age"),
       },
       {
@@ -117,7 +124,42 @@ export default class PanelTable extends Component {
         key: "address",
         ...this.getColumnSearchProps("address"),
       },
+      {
+        title: 'Status',
+        key: 'tags',
+        dataIndex: 'tags',
+        render: tags => (
+          <span>
+        {tags.map(tag => {
+          let color = 'blue';
+          if (tag === 'gender') {
+            color = 'volcano';
+          }
+          if (tag === 'prerequistics') {
+            color = 'red';
+          }
+          if (tag === 'eligible') {
+            color = 'green';
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+        ),
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+          <Popconfirm title="Are you sure you want to Remove this student?">
+            <a href="javascript:;">Remove</a>
+          </Popconfirm>
+        ),
+      },
     ];
-    return <Table columns={columns} dataSource={data} />;
+    return <Table columns={columns} dataSource={data}/>;
   }
 }
