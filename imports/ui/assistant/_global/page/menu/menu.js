f5d7b0e12b58c3667b3938b7d556ab99af0d7f48
Parsa Alian <Parsa@Bardias-MacBook-Pro.local>
2019-06-15 12:30:32 +0430

    assistant persian menu

---
+++ imports/ui/assistant/_global/page/menu/menu.js
@@ -0,0 +1,85 @@
+import { Meteor } from 'meteor/meteor';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { Layout, Menu, Icon, Avatar } from "antd";
+
+class TopMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { collapsed: false };
+    this.onCollapse = this.onCollapse.bind(this);
+    this.onClick = this.onClick.bind(this);
+    this.logout = this.logout.bind(this);
+  }
+
+  onCollapse(collapsed) {
+    this.setState({ collapsed });
+  }
+
+  onClick(e) {
+    this.props.history.push('/assistant' + e.key);
+  }
+
+  logout(e) {
+    Meteor.logout();
+    this.props.history.push('/');
+  }
+
+  render() {
+    return(
+      <React.Fragment>
+        <div style={{position: 'fixed', zIndex: '100', width: '100%'}}>
+          <Menu className="rtl" theme="dark" defaultSelectedKeys={[this.props.history.location.pathname.replace('/assistant', '')]} mode="horizontal" style={{ lineHeight: '64px' }}>
+            <Menu.Item className="float-right">
+              <img style={{height: '54px'}} src='/images/logow.png' />
+            </Menu.Item>
+
+            <Menu.Item className="float-right" key="/courses" onClick={this.onClick}>
+              <Icon type="book" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
+              <span style={{fontFamily: 'iransans', fontWeight: '600'}}>درس‌ها</span>
+            </Menu.Item>
+
+            <Menu.Item className="float-right" key="/students" onClick={this.onClick}>
+              <Icon type="team" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
+              <span style={{fontFamily: 'iransans', fontWeight: '600'}}>دانشجویان</span>
+            </Menu.Item>
+
+            <Menu.Item className="float-right" key="/chart" onClick={this.onClick}>
+              <Icon type="pie-chart" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
+              <span style={{fontFamily: 'iransans', fontWeight: '600'}}>جدول درسی</span>
+            </Menu.Item>
+
+            <Menu.Item key='/notifications' onClick={this.onClick}>
+              <Icon type="bell" theme="filled" style={{ fontSize: '1.5rem' }} />
+            </Menu.Item>
+
+            <Menu.SubMenu
+              style={{marginLeft: '10px'}}
+              title={
+                <React.Fragment>
+                  <Avatar size='large' style={{backgroundColor: '#00a2ae' }}><Icon style={{ margin: 0, position: 'relative', top: '-3px' }} type='user' /></Avatar>
+                  <Icon type="caret-down" style={{ position: 'relative', top: '-3px' }} />
+                </React.Fragment>
+              }>
+                <Menu.Item style={{ direction: 'rtl', margin: 0 }} key='/setting' onClick={this.onClick}>
+                  <span style={{ float: 'right' }}>
+                    <Icon type="setting" style={{ marginRight: 0 ,marginLeft: '10px' }} />
+                    <span style={{ fontFamily: 'iransans', fontWeight: '600' }}>تنظیمات</span>
+                  </span>
+                </Menu.Item>
+                <Menu.Item style={{ direction: 'rtl', margin: 0 }} onClick={this.logout}>
+                  <span style={{ float: 'right' }}>
+                    <Icon type="logout" style={{ marginRight: 0 ,marginLeft: '10px' }} />
+                    <span style={{ fontFamily: 'iransans', fontWeight: '600' }}>خروج</span>
+                  </span>
+                </Menu.Item>
+            </Menu.SubMenu>
+          </Menu>
+        </div>
+        <div style={{ lineHeight: '64px' }}>let this be here without reason</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withRouter(TopMenu);
--- imports/ui/assistant/_global/page/page.js
+++ imports/ui/assistant/_global/page/page.js
@@ -1,57 +1,12 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { Layout, Menu, Icon } from "antd";
-import "./page.css";
+import { Layout } from "antd";
+import Menu from './menu/menu';
 
-class Page extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { collapsed: false };
-    this.onCollapse = this.onCollapse.bind(this);
-    this.onClick = this.onClick.bind(this);
-  }
-
-  onCollapse(collapsed) {
-    this.setState({ collapsed });
-  }
-
-  onClick(e) {
-    console.log(this.props.history);
-    this.props.history.push('/assistant' + e.key);
-  }
-
+export default class Page extends Component {
   render() {
     return (
       <Layout style={{minHeight: '100vh'}}>
-        <div style={{position: 'fixed', zIndex: '100', width: '100%'}}>
-          <Menu className="rtl" theme="dark" defaultSelectedKeys={[this.props.history.location.pathname.replace('/assistant', '')]} mode="horizontal" style={{ lineHeight: '64px' }}>
-            <Menu.Item className="float-right" key="/" onClick={this.onClick} disabled={true}>
-              <Icon type="home" />
-              <span>خانه</span>
-            </Menu.Item>
-
-            <Menu.Item className="float-right" key="/courses" onClick={this.onClick}>
-              <Icon type="home" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
-              <span style={{fontFamily: 'IRANSans'}}>درس‌ها</span>
-            </Menu.Item>
-
-            <Menu.Item className="float-right" key="/students" onClick={this.onClick}>
-              <Icon type="team" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
-              <span>دانشجویان</span>
-            </Menu.Item>
-
-            <Menu.Item className="float-right" key="/chart" onClick={this.onClick}>
-              <Icon type="pie-chart" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
-              <span>جدول درسی</span>
-            </Menu.Item>
-
-            <Menu.Item className="float-right" key="/messages" onClick={this.onClick} disabled={true}>
-              <Icon type="notification" style={{ marginRight: 0, marginLeft: '10px', position: 'relative', top: '-3px' }} />
-              <span>پیام‌ها</span>
-            </Menu.Item>
-          </Menu>
-        </div>
-        <div style={{ lineHeight: '64px' }}>let this be here without reason</div>
+        <Menu />
 
         <Layout.Content style={{ padding: "24px" }}>
           {this.props.children}
@@ -64,5 +19,3 @@ class Page extends Component {
     );
   }
 }
-
-export default withRouter(Page);
