7055f6acf0f5a784373970f00407a599069b60f0
y-tabatabaee76 <yasamin.tabatbaee@gmail.com>
2019-06-16 00:01:10 +0430

    Adding filters

--- imports/ui/assistant/courses/filter/filter.js
+++ imports/ui/assistant/courses/filter/filter.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
-import { Form, Input, Option, Row, Col } from "antd";
+import React, {Component} from "react";
+import {Form, Input, Row, Col, Icon, Checkbox, Select} from "antd";
+
+const {Option} = Select;
 
 export default class Filter extends Component {
   render() {
@@ -7,25 +9,57 @@ export default class Filter extends Component {
       <React.Fragment>
         <Row style={{backgroundColor: 'white', padding: '24px', borderRadius: '6px', marginBottom: '12px'}}>
           <Row style={{margin: '12px'}}>
-            <h6 className='fa rtl float-right'>شماره‌ی درس</h6>
-            <Input />
+            <h6 className='fa rtl float-right'>فیلترها</h6>
           </Row>
           <Row style={{margin: '12px'}}>
-            <h6 className='fa rtl float-right'>نام درس</h6>
-            <Input />
+            <Checkbox>فقط دروس دارای ظرفیت</Checkbox>
+          </Row>
+          <Row style={{margin: '12px'}}>
+            <Select mode="multiple" placeholder="دانشکده" style={{width: '200px'}}>
+              <Option value="ce">مهندسی کامپیوتر</Option>
+              <Option value="cs">علوم کامپیوتر</Option>
+              <Option value="ee">مهندسی برق</Option>
+              <Option value="air"> مهندسی هوافضا</Option>
+            </Select>
+          </Row>
+          <Row style={{margin: '12px'}}>
+            <Select placeholder="مقطع" style={{width: '200px'}}>
+              <Option value="bsc"> کارشناسی</Option>
+              <Option value="msc">کارشناسی ارشد</Option>
+              <Option value="phd">دکترا</Option>
+            </Select>
           </Row>
         </Row>
 
         <Row style={{backgroundColor: 'white', padding: '24px', borderRadius: '6px', marginBottom: '12px'}}>
           <Row style={{margin: '12px'}}>
-            <h6 className='fa rtl float-right'>نام استاد</h6>
-            <Input />
+            <h6 className='fa rtl float-right'>مشخصات درس</h6>
+            <Input.Search
+              allowClear
+              placeholder="شماره درس"
+              onSearch={value => console.log(value)}
+            />
           </Row>
           <Row style={{margin: '12px'}}>
-            <h6 className='fa rtl float-right'></h6>
-            <Input />
+            <Input.Search
+              allowClear
+              placeholder="نام درس"
+              onSearch={value => console.log(value)}
+            />
           </Row>
         </Row>
+
+        <Row style={{backgroundColor: 'white', padding: '24px', borderRadius: '6px', marginBottom: '12px'}}>
+          <Row style={{margin: '12px'}}>
+            <h6 className='fa rtl float-right'>مشخصات استاد</h6>
+            <Input.Search
+              allowClear
+              placeholder="نام استاد"
+              onSearch={value => console.log(value)}
+            />
+          </Row>
+        </Row>
+
       </React.Fragment>
     );
   }
