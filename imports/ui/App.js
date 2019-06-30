c9e410767830701a6431d6a6b2e7172faf7cc193
y-tabatabaee76 <yasamin.tabatbaee@gmail.com>
2019-06-30 16:51:53 +0430

    Student Menu

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,7 +1,7 @@
-import { Meteor } from "meteor/meteor";
-import React, { Component } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { Students } from "/imports/api/students/students";
+import {Meteor} from "meteor/meteor";
+import React, {Component} from "react";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {Students} from "/imports/api/students/students";
 
 import Login from "./login/login";
 import NotFound from "./_global/404";
@@ -15,6 +15,7 @@ import AssistantCourses from "./assistant/courses/courses";
 import AssistantCoursePanel from "./assistant/panel/panel";
 // student panel pages
 import StudentHome from "./students/home";
+import StudentCourses from "./students/courses/courses";
 
 export default class App extends Component {
   render() {
@@ -23,17 +24,18 @@ export default class App extends Component {
         <Switch>
           <Route path="/" exact component={Login}/>
 
-          <Route path="/admin" exact component={AdminHome} />
+          <Route path="/admin" exact component={AdminHome}/>
           <Route path="/admin/students" exact component={AdminStudents}/>
           <Route path="/admin/courses" exact component={AdminCourses}/>
 
-          <Route path="/assistant" exact component={AssistantHome} />
-          <Route path="/assistant/courses" exact component={AssistantCourses} />
-          <Route path="/assistant/courses/:id" exact component={AssistantCoursePanel} />
+          <Route path="/assistant" exact component={AssistantHome}/>
+          <Route path="/assistant/courses" exact component={AssistantCourses}/>
+          <Route path="/assistant/courses/:id" exact component={AssistantCoursePanel}/>
 
-          <Route path="/students" exact component={StudentHome} />
+          <Route path="/students" exact component={StudentHome}/>
+          <Route path="/students/courses" exact component={StudentCourses}/>
 
-          <Route exact component={NotFound} />
+          <Route exact component={NotFound}/>
         </Switch>
       </BrowserRouter>
     );
--- imports/ui/students/home.js
+++ imports/ui/students/home.js
@@ -2,7 +2,7 @@ import { Meteor } from "meteor/meteor";
 import React, { Component } from "react";
 import Page from "./courses/courses";
 
-export default class AssistantPage extends Component {
+export default class StudentHome extends Component {
   render() {
     return <Page></Page>;
   }
