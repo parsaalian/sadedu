cb6c2e505b844cd3b6031637ccf65470bee810a0
y-tabatabaee76 <yasamin.tabatbaee@gmail.com>
2019-07-04 18:36:53 +0430

    Registration Table

--- imports/ui/students/registration/form/form.js
+++ imports/ui/students/registration/form/form.js
@@ -9,14 +9,14 @@ class RegistrationForm extends Component {
       <React.Fragment>
         <Form layout="vertical" hideRequiredMark>
           <Row gutter={16}>
-            <Col span={4}>
+            <Col span={8}>
               <Form.Item label="شماره درس">
                 {getFieldDecorator("name", {
                   rules: [{required: true, message: "لطفا شماره درس را وارد نمایید!"}],
                 })(<Input placeholder="شماره درس" maxLength={50}/>)}
               </Form.Item>
             </Col>
-            <Col span={4}>
+            <Col span={8}>
               <Form.Item label="شماره گروه">
                 {(<InputNumber defaultValue="1" min={1} max={10}/>)}
               </Form.Item>
@@ -26,7 +26,10 @@ class RegistrationForm extends Component {
                 {(<InputNumber defaultValue="3" min={0} max={4}/>)}
               </Form.Item>
             </Col>
-            <Col span={4}>
+          </Row>
+
+          <Row gutter={16} >
+            <Col span={4} >
               <Button type="primary">
                 <Icon type="form-o"/>ثبت نام
               </Button>
