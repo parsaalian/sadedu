5bfe714d03ab71ac6e72e6f709809fbddcd35b20
y-tabatabaee76 <yasamin.tabatbaee@gmail.com>
2019-07-04 18:26:01 +0430

    Registration form

--- imports/ui/students/registration/registration.js
+++ imports/ui/students/registration/registration.js
@@ -1,21 +1,25 @@
 import React, {Component} from "react";
 import Page from "/imports/ui/students/_global/page/page";
+import Form from "/imports/ui/students/registration/form/form"
+import Table from "/imports/ui/students/registration/table/table"
 import {Row, Col} from "antd";
 
 export default class Registration extends Component {
   render() {
     return (
       <Page>
-        <Row>
-          <Col span={18}>
-            <Row type="flex" justify="center">
-              <Col span={18}>
-
+        <Row type="flex" justify="center">
+          <Col span={16} lg={18} md={22} sm={24} xs={24}>
+            <Row type="flex" justify="center" style={{background: "#fff", padding: "24px"}}>
+              <Col span={18} style={{fontFamily: "iransans", fontWeight: "600"}}>
+                <Form></Form>
               </Col>
             </Row>
-          </Col>
-          <Col span={6} style={{position: "fixed", right: "32px"}}>
-
+            <Row type="flex" justify="center" style={{background: "#fff", padding: "24px"}}>
+              <Col span={18} style={{fontFamily: "iransans", fontWeight: "600"}}>
+                <Table></Table>
+              </Col>
+            </Row>
           </Col>
         </Row>
       </Page>
